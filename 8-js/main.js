d8ed58a9f5d54d2541e67068cefade94eef9f4d8
azamjon08 <104623940+azamjon08@users.noreply.github.com>
2022-04-29 15:57:37 +0500

    Add files via upload

---
+++ 8-js/main.js
@@ -0,0 +1,17 @@
+let link = document.querySelector ('.link')
+
+link.addEventListener ('click' , function(){
+    link.style.transform = 'scale()'
+    link.style.marginLeft = '700px'
+    link.style.marginTop = '300px'
+    link.classList = 'boy'
+    link.classList = 'boy2'
+})
+
+
+link.addEventListener ('click' , function(){
+    link.style.transform = 'scale()'
+    link.classList = 'boy2'
+    link.classList = 'boy'
+    
+})
